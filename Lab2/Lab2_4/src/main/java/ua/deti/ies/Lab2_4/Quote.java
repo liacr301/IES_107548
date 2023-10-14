720a11b629749f9fd29dae78135741e1820421b7
--global <--global>
2023-10-14 19:04:13 +0100

    Lab 2 completed.

--- Lab2/Lab2_4/src/main/java/ua/deti/ies/Lab2_4/Quote.java
+++ Lab2/Lab2_4/src/main/java/ua/deti/ies/Lab2_4/Quote.java
@@ -1,3 +1,3 @@
 package ua.deti.ies.Lab2_4;
 
-public record Quote(long id, String content){}
+public record Quote(int show_id, String content){}
--- Lab2/Lab2_4/src/main/java/ua/deti/ies/Lab2_4/QuotesRestController.java
+++ Lab2/Lab2_4/src/main/java/ua/deti/ies/Lab2_4/QuotesRestController.java
@@ -7,49 +7,54 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 public class QuotesRestController {
 
     private static final String template = "Quote: %s!";
     private final AtomicLong counter_quote = new AtomicLong();
+    HashMap<Integer, String> quotes = new HashMap<Integer, String>();
+    HashMap<Integer, String> shows = new HashMap<Integer, String>();
 
-    //this one is working
+    public QuotesRestController() {
+        quotes.put(1, "Now I Know What A TV Dinner Feels Like.");
+        quotes.put(1, "Does It Sound Like I'm Ordering A Pizza?");
+        quotes.put(2, "She begged for her life...");
+        quotes.put(2, "What's in the box?");
+        quotes.put(3, "We always called each other Goodfellas.");
+        quotes.put(4, "What I need is a real, live ghost.");
+        quotes.put(4, "Solving mysteries was a lot more fun than selling them.");
+
+        shows.put(1, "Die Hard");
+        shows.put(2, "Seven");
+        shows.put(3, "Goodfellas");
+        shows.put(4, "Scooby-Doo on Zombie Island");
+    }
     @GetMapping("/quote")
     public Quote quote() {
-        return new Quote(counter_quote.incrementAndGet(), String.format(template, "Say hello to my little friend!"));
+        int r = (int) (Math.random() * (7 - 1)) + 1;
+        String rand_quote = quotes.get(r);
+        return new Quote(r, String.format(template, rand_quote));
     }
 
-    //this one is working too
+
+    //this one is working too ----> going to change
     @GetMapping("/shows")
     public List<Show> shows() {
-        Show show_1 = new Show(1, "Die Hard");
-        Show show_2 = new Show(2, "Goodfellas");
-        Show show_3 = new Show(3, "Seven");
-        Show show_4 = new Show(4, "Scooby-Doo on Zombie Island");
-
-        List<Show> shows_all = new ArrayList<Show>();
-        shows_all.add(show_1);
-        shows_all.add(show_2);
-        shows_all.add(show_3);
-        shows_all.add(show_4);
-
+        List<Show> shows_all = new ArrayList<>();
+        for (int i = 1; i < shows.size()+1; i++){
+            Show new_show = new Show(i, shows.get(i));
+            shows_all.add(new_show);
+        }
         return shows_all;
     }
 
     @GetMapping("/quotes?show=<show_id>")
-    public Greeting shows(@RequestParam(value = "show", defaultValue = "None") String show) {
-        HashMap<Show,String> show_quotes = new HashMap<>();
-        show_quotes.put((1,"Die Hard"),"Nice Suit. John Philips, London. I Have Two Myself.");
-        show_quotes.put((1,"Die Hard"),"Now I Know What A TV Dinner Feels Like.");
-        show_quotes.put((1,"Die Hard"),"Does It Sound Like I'm Ordering A Pizza?");
-        show_quotes.put((2,"Goodfellas"),"We always called each other Goodfellas.");
-        show_quotes.put((3,"Seven"),"She begged for her life...");
-        show_quotes.put((3,"Seven"),"What's in the box?");
-        show_quotes.put((3,"Seven"),"David. If you kill him, he will win.");
-        show_quotes.put((3,"Seven"),"This isn't going to have a happy ending.");
-        show_quotes.put((4,"Scooby-Doo on Zombie Island"),"What I need is a real, live ghost.");
-        show_quotes.put((4,"Scooby-Doo on Zombie Island"),"Solving mysteries was a lot more fun than selling them.");
-        return
+    public List<Quote> specificShow(@RequestParam("show_id") int showId) {
+        List<Quote> quotesShow = new ArrayList<>();
+        // Logic to retrieve quotes for the specified show_id
+        return quotesShow;
     }
+
 }
